3ac85bf9754676403401ef636b4fba4b96ced3f7
chaima staifa <chaimastaifa@gmail.com>
2023-08-10 10:12:35 -0700

    mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -5,45 +5,45 @@
 #define ERR_MSG "Error"
 
 /**
- * check_digit - checks if a string contains only digits
+ * is_digit - checks if a string contains a non-digit char
  * @s: string to be evaluated
  *
- * Return: 0 if a non-digit character is found, 1 otherwise
+ * Return: 0 if a non-digit is found, 1 otherwise
  */
-int check_digit(char *string)
+int is_digit(char *s)
 {
-	int index = 0;
+	int i = 0;
 
-	while (string[index])
+	while (s[i])
 	{
-		if (string[index] < '0' || string[index] > '9')
+		if (s[i] < '0' || s[i] > '9')
 			return (0);
-		index++;
+		i++;
 	}
 	return (1);
 }
 
 /**
- * get_str_length - calculates the length of a string
+ * _strlen - returns the length of a string
  * @s: string to evaluate
  *
  * Return: the length of the string
  */
-int get_str_length(char *string)
+int _strlen(char *s)
 {
-	int index = 0;
+	int i = 0;
 
-	while (string[index] != '\0')
+	while (s[i] != '\0')
 	{
-		index++;
+		i++;
 	}
-	return (index);
+	return (i);
 }
 
 /**
- * handle_errors - handles errors for the main function
+ * errors - handles errors for main
  */
-void handle_errors(void)
+void errors(void)
 {
 	printf("Error\n");
 	exit(98);
@@ -58,27 +58,27 @@ void handle_errors(void)
  */
 int main(int argc, char *argv[])
 {
-	char *str1, *str2;
-	int len1, len2, total_len, i, carry, digit1, digit2, *result, a = 0;
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-	str1 = argv[1], str2 = argv[2];
-	if (argc != 3 || !check_digit(str1) || !check_digit(str2))
-		handle_errors();
-	len1 = get_str_length(str1);
-	len2 = get_str_length(str2);
-	total_len = len1 + len2 + 1;
-	result = malloc(sizeof(int) * total_len);
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
 	if (!result)
 		return (1);
 	for (i = 0; i <= len1 + len2; i++)
 		result[i] = 0;
 	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		digit1 = str1[len1] - '0';
+		digit1 = s1[len1] - '0';
 		carry = 0;
-		for (len2 = get_str_length(str2) - 1; len2 >= 0; len2--)
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
-			digit2 = str2[len2] - '0';
+			digit2 = s2[len2] - '0';
 			carry += result[len1 + len2 + 1] + (digit1 * digit2);
 			result[len1 + len2 + 1] = carry % 10;
 			carry /= 10;
@@ -86,7 +86,7 @@ int main(int argc, char *argv[])
 		if (carry > 0)
 			result[len1 + len2 + 1] += carry;
 	}
-	for (i = 0; i < total_len - 1; i++)
+	for (i = 0; i < len - 1; i++)
 	{
 		if (result[i])
 			a = 1;
